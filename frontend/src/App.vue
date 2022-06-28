d403c5607ae3fe61f20cd707df33e2e403d5195e
Shuhei2455 <skirigaya98@.com>
2022-06-29 01:24:04 +0900

    first_commit

---
+++ frontend/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+    <v-app>
+    <div 
+      id="nav" 
+      class="tab-area-base">
+      <ul class="tab-menu-base">
+        <!-- `router.js` で定義したルーティングルールとの紐付けを行っている -->
+        <li><router-link to="/">Home</router-link></li>
+        <!-- *** -->
+        <!-- 省略 -->
+        <!-- *** -->
+        <li><router-link to="/Test">Rounting Test</router-link></li>
+      </ul>
+    </div>
+    </v-app>
+</template>
+
+<script>
+export default {
+
+};
+</script>
---
+++ frontend/src/components/Test.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="test">
+        <h1>{{ msg }}</h1>
+        <router-link to="/">Home</router-link>
+        <router-link to="/test">testページ</router-link>        
+    </div>
+</template>
+ 
+<script>
+    export default {
+        name: 'test',
+        data () {
+            return {
+                msg: 'テストページです'
+            }
+        }
+    }
+</script> 
+
+<style scoped>
+    h1 {
+        margin: 40px 0 0;
+        color: #41B883;
+    }
+ 
+    a {
+        color: #41B883;
+    }
+</style>
